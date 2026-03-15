f93c70533d92995ddb599cfc318cdbe0c28564c7
Addy3131 <117356825+Addy3131@users.noreply.github.com>
2026-03-15 20:45:18 +0530

    Initial InfraDesk monorepo

---
+++ infradesk-deploy/start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Set correct permissions
+chown -R www-data:www-data /var/www/backend/storage /var/www/backend/bootstrap/cache
+chmod -R 775 /var/www/backend/storage /var/www/backend/bootstrap/cache
+
+# Start Supervisord
+exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
